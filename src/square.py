4efe51c5bea754f77ebeec6f8d1b3e327d467821
Cezar Crintea <cezar.crintea@customprojects.ro>
2023-05-17 11:30:33 +0300

    Added VSCode settings esp linting & formatting. Added Square class

---
+++ src/square.py
@@ -0,0 +1,10 @@
+"""Contains the square class"""
+
+
+class Square:
+    """Contains the information for a field's square"""
+
+    def __init__(self):
+        self.mine = False
+        self.mask = True
+        self.flag = False
